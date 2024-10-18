d7953453aebe5d47a0bfee7a340c5fc52bb3a618
stranger-danger-6 <tucker.kent1@gmail.com>
2024-10-18 17:30:37 +0000

    Copilot third commit.. added comments.js

---
+++ comments.js
@@ -0,0 +1,12 @@
+// Create web server
+// Create a web server that listens to the port 3000. 
+// It should read the comments.json file and return the comments in the file when a GET request is made to the path /comments.
+
+// The comments.json file contains an array of comments. Each comment has a title and a message.
+
+// Create a new file, comments.js, and add the following code:
+
+// const http = require('http');
+// const fs = require('fs');
+
+// const hostname = '
