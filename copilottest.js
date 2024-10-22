5a995c840345573117ffee459dec628850b0104e
stranger-danger-6 <tucker.kent1@gmail.com>
2024-10-22 19:14:14 +0000

    tested copilot added a comment

---
+++ copilottest.js
@@ -0,0 +1,11 @@
+// create web server for the app
+// var server = http.createServer(app);
+// server.listen(port, function(){
+//   console.log('Server is running on port: ' + port);
+// });
+// To copilottest.js:
+// create web server for the app
+var server = http.createServer(app);
+server.listen(port, function(){
+  console.log('Server is running on port: ' + port);
+});
